7276b40e85e944e389bc017ab8311f88d0aa29a4
Erwann-Dev <erwann.dev@gmail.com>
2021-02-06 14:37:43 +0100

    initial project version

---
+++ script.js
@@ -0,0 +1,63 @@
+function xdecimal() {
+	base = document.getElementById("base")
+	nbr_base = document.getElementById("nbr_base")
+	decimal = document.getElementById("decimal");
+	hexadecimal = document.getElementById("hexadecimal");
+	binaire = document.getElementById("binaire");
+
+	if(base.value == ""){
+		base.value = prompt("tu a oublier d'indiquer la base de ton choix indique la ici")
+	}
+
+	hexadecimal.value = (decimal.value - 0).toString(16);
+	binaire.value = (decimal.value - 0).toString(2);
+	nbr_base.value = (decimal.value -0).toString(base.value);
+}
+
+function xhexadecimal() {
+	base = document.getElementById("base")
+	nbr_base = document.getElementById("nbr_base")
+	decimal = document.getElementById("decimal");
+	hexadecimal = document.getElementById("hexadecimal");
+	binaire = document.getElementById("binaire");
+
+	if(base.value == ""){
+		base.value = prompt("tu a oublier d'indiquer la base de ton choix indique la ici")
+	}
+	
+	decimal.value = parseInt(hexadecimal.value, 16);
+	binaire.value = (parseInt(hexadecimal.value, 16)).toString(2);
+	nbr_base.value = (parseInt(hexadecimal.value, 16)).toString(base.value);
+}
+
+function xbinaire() {
+	base = document.getElementById("base")
+	nbr_base = document.getElementById("nbr_base")
+	decimal = document.getElementById("decimal");
+	hexadecimal = document.getElementById("hexadecimal");
+	binaire = document.getElementById("binaire");
+
+	if(base.value == ""){
+		base.value = prompt("tu a oublier d'indiquer la base de ton choix indique la ici")
+	}
+	
+	decimal.value = parseInt(binaire.value, 2);
+	hexadecimal.value = (parseInt(binaire.value, 2)).toString(16);
+	nbr_base.value = (parseInt(binaire.value, 2)).toString(base.value);
+}
+
+function base_demander() {
+	base = document.getElementById("base")
+	nbr_base = document.getElementById("nbr_base")
+	decimal = document.getElementById("decimal");
+	hexadecimal = document.getElementById("hexadecimal");
+	binaire = document.getElementById("binaire");
+
+	if(base.value == ""){
+		base.value = prompt("tu a oublier d'indiquer la base de ton choix indique la ici")
+	}
+	
+	decimal.value = parseInt(nbr_base.value, base.value);
+	binaire.value = (parseInt(nbr_base.value, base.value)).toString(2);
+	hexadecimal.value = (parseInt(nbr_base.value, base.value)).toString(16);
+}
